ceb483099804adc2c12df36415cc43fe355e6e33
RunningBrute <piotrekbas8@gmail.com>
2026-01-09 21:36:21 +0100

    add simple reading for garmin fit files

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+#include <fit_decode.hpp>
+#include <fit_mesg.hpp>
+
+#include <fstream>
 #include <iostream>
+
 #include "iris/iris.hpp"
 #include "iris/task.hpp"
 
@@ -8,10 +13,34 @@ iris::Task run()
     co_return;
 }
 
+struct DummyListener : fit::MesgListener
+{
+    void OnMesg(fit::Mesg&) override {}
+};
+
 int main()
 {
     std::cout << "Iris starting...\n";
     run();
 
+    std::ifstream file("data/sample.fit", std::ios::binary);
+    if (!file)
+    {
+        std::cerr << "Cannot open FIT file\n";
+        return 1;
+    }
+
+    fit::Decode decoder;
+    DummyListener listener;
+
+    std::cout << "Decoding FIT file...\n";
+
+    if (!decoder.Read(file, listener))
+    {
+        std::cerr << "FIT decode failed\n";
+        return 1;
+    }
+
+    std::cout << "FIT decode OK\n";
     return 0;
 }
